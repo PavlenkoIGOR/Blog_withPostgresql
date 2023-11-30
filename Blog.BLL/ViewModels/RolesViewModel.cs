636fa3ffba7598d9feed938b07845df03075f84e
PavlenkoIGOR <illusiopard@gmail.com>
2023-12-01 00:47:03 +0300

    проверка по ролям проходит.

---
+++ Blog.BLL/ViewModels/RolesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.BLL.ViewModels
+{
+    public enum RolesViewModel
+    {
+        [Display(Name = "User")]
+        User,
+
+        [Display(Name = "Moderator")]
+        Moderator,
+
+        [Display(Name = "Administrator")]
+        Administrator
+    }
+}
